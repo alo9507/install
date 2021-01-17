da4032b696a1428706f61414c4386645f07b286d
mktcode <kontakt@markus-kottlaender.de>
2021-01-17 20:45:36 +0100

    fixed post deploy script

--- post-deploy.js
+++ post-deploy.js
@@ -1,9 +1,9 @@
 const sh = require('shelljs')
 
-const octoBayAddress = require('./contracts/build/contracts/OctoBay.json').address
-const octoPinAddress = require('./contracts/build/contracts/OctoPin.json').address
-const linkAddress = require('./contracts/build/contracts/LinkToken.json').address
-const paymasterAddress = require('./contracts/build/contracts/OctoBayPaymaster.json').address
+const octoBayAddress = Object.values(require('./contracts/build/contracts/OctoBay.json').networks)[0].address
+const octoPinAddress = Object.values(require('./contracts/build/contracts/OctoPin.json').networks)[0].address
+const linkAddress = Object.values(require('./contracts/build/contracts/LinkToken.json').networks)[0].address
+const paymasterAddress = Object.values(require('./contracts/build/contracts/OctoBayPaymaster.json').networks)[0].address
 
 // .env
 sh.sed('-i', /^OCTOBAY_ADDRESS=.*$/, 'OCTOBAY_ADDRESS=' + octoBayAddress, './app/.env')
