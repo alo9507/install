2f295782c417df32fa197109121e60436da53bee
mktcode <kontakt@markus-kottlaender.de>
2021-01-17 18:30:48 +0100

    fixed twitter job creation

--- create-chainlink-jobs.js
+++ create-chainlink-jobs.js
@@ -30,8 +30,8 @@ sh.echo('Creating jobs...')
 sh.sed('-i', /\"name\": \".*\"/, `"name": "OctoBay Register ${timestamp}"`, './.jobs/register.json')
 sh.sed('-i', /\"name\": \".*\"/, `"name": "OctoBay Release ${timestamp}"`, './.jobs/release.json')
 sh.sed('-i', /\"name\": \".*\"/, `"name": "OctoBay Claim ${timestamp}"`, './.jobs/claim.json')
-sh.sed('-i', /\"name\": \".*\"/, `"name": "OctoBay Claim ${timestamp}"`, './.jobs/twitter-post.json')
-sh.sed('-i', /\"name\": \".*\"/, `"name": "OctoBay Claim ${timestamp}"`, './.jobs/twitter-followers.json')
+sh.sed('-i', /\"name\": \".*\"/, `"name": "OctoBay Twitter Post ${timestamp}"`, './.jobs/twitter-post.json')
+sh.sed('-i', /\"name\": \".*\"/, `"name": "OctoBay Twitter Followers ${timestamp}"`, './.jobs/twitter-followers.json')
 
 // replacing oracle address
 sh.sed('-i', /\"address\": \".*\"/, `"address": "${oracleAddress}"`, './.jobs/register.json')
@@ -44,13 +44,15 @@ sh.sed('-i', /\"address\": \".*\"/, `"address": "${oracleAddress}"`, './.jobs/tw
 sh.exec('chainlink jobs create ./.jobs/register.json')
 sh.exec('chainlink jobs create ./.jobs/release.json')
 sh.exec('chainlink jobs create ./.jobs/claim.json')
+sh.exec('chainlink jobs create ./.jobs/twitter-post.json')
+sh.exec('chainlink jobs create ./.jobs/twitter-followers.json')
 
 // store job ids in filenames
 sh.exec('chainlink jobs list', (code, output) => {
   const jobIDs = output.match(/[a-f0-9]{32}/ig).reverse()
-  sh.sed('-i', /^CHAINLINK_TWITTER_FOLLOWERS_JOB_ID=.*$/, 'CHAINLINK_TWITTER_FOLLOWERS_JOB_ID=' + jobIDs[4], 'contracts/.env')
-  sh.sed('-i', /^CHAINLINK_TWITTER_POST_JOB_ID=.*$/, 'CHAINLINK_TWITTER_POST_JOB_ID=' + jobIDs[3], 'contracts/.env')
-  sh.sed('-i', /^CHAINLINK_CLAIM_JOB_ID=.*$/, 'CHAINLINK_CLAIM_JOB_ID=' + jobIDs[0], 'contracts/.env')
-  sh.sed('-i', /^CHAINLINK_RELEASE_JOB_ID=.*$/, 'CHAINLINK_RELEASE_JOB_ID=' + jobIDs[1], 'contracts/.env')
-  sh.sed('-i', /^CHAINLINK_REGISTER_JOB_ID=.*$/, 'CHAINLINK_REGISTER_JOB_ID=' + jobIDs[2], 'contracts/.env')
+  sh.sed('-i', /^CHAINLINK_TWITTER_FOLLOWERS_JOB_ID=.*$/, 'CHAINLINK_TWITTER_FOLLOWERS_JOB_ID=' + jobIDs[0], './contracts/.env')
+  sh.sed('-i', /^CHAINLINK_TWITTER_POST_JOB_ID=.*$/, 'CHAINLINK_TWITTER_POST_JOB_ID=' + jobIDs[1], './contracts/.env')
+  sh.sed('-i', /^CHAINLINK_CLAIM_JOB_ID=.*$/, 'CHAINLINK_CLAIM_JOB_ID=' + jobIDs[2], './contracts/.env')
+  sh.sed('-i', /^CHAINLINK_RELEASE_JOB_ID=.*$/, 'CHAINLINK_RELEASE_JOB_ID=' + jobIDs[3], './contracts/.env')
+  sh.sed('-i', /^CHAINLINK_REGISTER_JOB_ID=.*$/, 'CHAINLINK_REGISTER_JOB_ID=' + jobIDs[4], './contracts/.env')
 })
