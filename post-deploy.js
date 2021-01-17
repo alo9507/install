bbcfe63b964a07e54b1319dc5eb42c19453eb172
mktcode <kontakt@markus-kottlaender.de>
2021-01-17 17:11:20 +0100

    fixed script dirs

--- post-deploy.js
+++ post-deploy.js
@@ -1,12 +1,12 @@
 const sh = require('shelljs')
 
-const octoBayAddress = require('./.evm/build/contracts/OctoBay.json').address
-const octoPinAddress = require('./.evm/build/contracts/OctoPin.json').address
-const linkAddress = require('./.evm/build/contracts/LinkToken.json').address
-const paymasterAddress = require('./.evm/build/contracts/OctoBayPaymaster.json').address
+const octoBayAddress = require('./contracts/build/contracts/OctoBay.json').address
+const octoPinAddress = require('./contracts/build/contracts/OctoPin.json').address
+const linkAddress = require('./contracts/build/contracts/LinkToken.json').address
+const paymasterAddress = require('./contracts/build/contracts/OctoBayPaymaster.json').address
 
 // .env
-sh.sed('-i', /^OCTOBAY_ADDRESS=.*$/, 'OCTOBAY_ADDRESS=' + octoBayAddress, './.app/.env')
-sh.sed('-i', /^OCTOPIN_ADDRESS=.*$/, 'OCTOPIN_ADDRESS=' + octoPinAddress, './.app/.env')
-sh.sed('-i', /^GSN_PAYMASTER_ADDRESS=.*$/, 'GSN_PAYMASTER_ADDRESS=' + paymasterAddress, './.app/.env')
-sh.sed('-i', /^LINK_CONTRACT_ADDRESS=.*$/, 'LINK_CONTRACT_ADDRESS=' + linkAddress, './.chainlink/node/.env')
+sh.sed('-i', /^OCTOBAY_ADDRESS=.*$/, 'OCTOBAY_ADDRESS=' + octoBayAddress, './app/.env')
+sh.sed('-i', /^OCTOPIN_ADDRESS=.*$/, 'OCTOPIN_ADDRESS=' + octoPinAddress, './app/.env')
+sh.sed('-i', /^GSN_PAYMASTER_ADDRESS=.*$/, 'GSN_PAYMASTER_ADDRESS=' + paymasterAddress, './app/.env')
+sh.sed('-i', /^LINK_CONTRACT_ADDRESS=.*$/, 'LINK_CONTRACT_ADDRESS=' + linkAddress, './chainlink/.env')
--- pre-deploy.js
+++ pre-deploy.js
@@ -1,4 +1,4 @@
-require('dotenv').config({ path: './.evm/.env' })
+require('dotenv').config({ path: './contracts/.env' })
 const readline = require('readline')
 const sh = require('shelljs')
 
@@ -7,16 +7,16 @@ const rl = readline.createInterface({
   output: process.stdout
 })
 
-const relayHubAddress = require('./.evm/build/gsn/RelayHub.json').address
-const forwarderAddress = require('./.evm/build/gsn/Forwarder.json').address
+const relayHubAddress = require('./contracts/build/gsn/RelayHub.json').address
+const forwarderAddress = require('./contracts/build/gsn/Forwarder.json').address
 
 sh.sed('-i', /^GSN_RELAYHUB_ADDRESS=.*$/, 'GSN_RELAYHUB_ADDRESS=' + relayHubAddress, './app/.env')
-sh.sed('-i', /^GSN_RELAYHUB_ADDRESS=.*$/, 'GSN_RELAYHUB_ADDRESS=' + relayHubAddress, '.evm/.env')
-sh.sed('-i', /^GSN_FORWARDER_ADDRESS=.*$/, 'GSN_FORWARDER_ADDRESS=' + forwarderAddress, '.evm/.env')
+sh.sed('-i', /^GSN_RELAYHUB_ADDRESS=.*$/, 'GSN_RELAYHUB_ADDRESS=' + relayHubAddress, './contracts/.env')
+sh.sed('-i', /^GSN_FORWARDER_ADDRESS=.*$/, 'GSN_FORWARDER_ADDRESS=' + forwarderAddress, './contracts/.env')
 
 if (!process.env.CHAINLINK_NODE_ADDRESS) {
   rl.question('Chainlink Node Address? ', chainlinkNodeAddress => {
-    sh.sed('-i', /^CHAINLINK_NODE_ADDRESS=.*$/, 'CHAINLINK_NODE_ADDRESS=' + chainlinkNodeAddress, '.evm/.env')
+    sh.sed('-i', /^CHAINLINK_NODE_ADDRESS=.*$/, 'CHAINLINK_NODE_ADDRESS=' + chainlinkNodeAddress, './contracts/.env')
     rl.close()
   })
 } else {
